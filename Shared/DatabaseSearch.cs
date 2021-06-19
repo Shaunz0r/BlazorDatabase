4e6c13081cb0dda985763a80d2dc2faeb09d7262
Shaunz0r <shaunz0r@gmail.com>
2021-06-19 14:52:11 +0100

    vs code commit test

---
+++ Shared/DatabaseSearch.cs
@@ -0,0 +1,13 @@
+public class DatabaseSearch
+{
+    public int StaffId { get; set; }
+    public string Name { get; set; }
+    public string Department { get; set; } 
+
+    public DatabaseSearch()
+    {
+        StaffId = StaffId;
+        Name = Name;
+        Department = Department;
+    }
+}
