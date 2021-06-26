180717229820caeff81e25c450f15b89272b397c
Shaunz0r <shaunz0r@gmail.com>
2021-06-26 16:23:55 +0100

    Add project files.

---
+++ newTest/Data/PersonnelService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using newTest.Data.newTest;
+
+namespace newTest.Data
+{
+    public class PersonnelService
+    {
+        private readonly CompanydirectoryContext _context;
+        public PersonnelService(CompanydirectoryContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<PersonnelNew>> GetPersonnelAsync(string strCurrentUser)
+        {
+            return await _context.PersonnelNew
+                .Where(h => h.UserName == strCurrentUser)
+                .AsNoTracking().ToListAsync();
+        }
+
+        
+    }
+}
